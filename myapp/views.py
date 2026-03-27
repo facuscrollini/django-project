376d2ca0b7b1c886bb55c0c493bd301c342c90a6
facuscrollini <facuscrollinic@gmail.com>
2026-03-27 18:19:14 +0100

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hola lola</h1>")
+
+
+
+def about(request):
+    return HttpResponse("<h3>About</h3>")
