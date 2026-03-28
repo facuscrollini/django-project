a847d52aee8f77df005ffcfda5ff34af203be3c8
facuscrollini <facuscrollinic@gmail.com>
2026-03-28 14:51:10 +0100

    agregado modulo myapp y configurado rutas basicas

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,36 @@
 from django.http import HttpResponse
+from .models import Task, Project
+
+def index(request):
+    return HttpResponse("<h1>Index Page </h1>")
+
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hola lola</h1>")
+def hello(request, id ):
+
+    # return HttpResponse(f"<h1>Hola {username} lola</h1>")
+    return HttpResponse(f"<h1>Hola {id} lola</h1>" )
 
 
 
 def about(request):
     return HttpResponse("<h3>About</h3>")
+
+
+
+def projects(request):
+
+    projects = Project.objects.all();
+
+    html = "<h1>Projects</h1><ul>"
+
+    for project in projects:
+        html += f"<li>{project.name}</li>"
+    
+    html += "</ul>"
+
+    return HttpResponse(html)
+
+
+def tasks(request):
+    return HttpResponse("<h1>Tasks</h1>")
